Fixed environment reward bugs: -v_x and quaternion similarity

entry_point_TD3_PR.py
@@ -51,13 +51,13 @@ action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n
 #                         log_path='TD3_eval_PR_logs_2/', eval_freq=50000,
 #                         deterministic=True, render=False)
 
-checkpoint_callback = CheckpointCallback(save_freq=10000, save_path='TD3_PR_checkpoint_models_3/')
+checkpoint_callback = CheckpointCallback(save_freq=10000, save_path='TD3_PR_checkpoint_models_4/')
 # callback = CallbackList([checkpoint_callback, eval_callback])
 
 model = TD3(CustomTD3Policy, 
             env, 
             gamma=0.99, 
-            learning_rate=3e-4, 
+            learning_rate=0.0001, 
             buffer_size=100000,
             learning_starts=10000,
             train_freq=100, 
@@ -68,7 +68,7 @@ model = TD3(CustomTD3Policy,
             target_policy_noise=0.2, 
             target_noise_clip=0.5, 
             random_exploration=0.0, 
-            tensorboard_log='TD3_experiment_PR_logs_3/',
+            tensorboard_log='TD3_experiment_PR_logs_4/',
             seed=1, 
             action_noise=action_noise, 
             verbose=1)

evaluate.py
@@ -3,6 +3,7 @@
 import gym
 import tensorflow as tf
 import numpy as np
+import pybullet as p
 
 from stable_baselines import TD3
 from stable_baselines.common.evaluation import evaluate_policy
@@ -11,18 +12,36 @@ from rex_gym.envs.rex_gym_env import RexGymEnv
 from rexPeriodicRewardEnv import rexPeriodicRewardEnv
 
 env = rexPeriodicRewardEnv(render=True)
+p = env.rex._pybullet_client
+model_id = env.rex.quadruped
+print('model ID: ', model_id)
+rex_joints = p.getNumJoints(bodyUniqueId=model_id)
+print('Number of Rex Joints = ', rex_joints)
+
+link_name_to_ID = {}
+for i in range(rex_joints):
+	name = p.getJointInfo(model_id, i)[12].decode('UTF-8')
+	link_name_to_ID[name] = i
 
 # Load the model
-model = TD3.load(load_path='Periodic Reward Logs/TD3_Run_1/TD3_PR_models/best_model', env=env)
+model = TD3.load(load_path='training_models/TD3_PR_checkpoint_models_3/rl_model_1420000_steps', env=env)
 
 # Evaluate the agent
 mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=1)
 
 # Enjoy trained agent (not really that enjoyable TBH)
 obs = env.reset()
+des = np.array([0,0,0,1])
 for i in range(1000):
     action, _states = model.predict(obs)
     obs, rewards, dones, info = env.step(action)
+    for l in link_name_to_ID.keys():
+        if 'shoulder' in l:
+            print('Link name = ', l)
+            orientation = np.array(p.getLinkState(bodyUniqueId=model_id, linkIndex=link_name_to_ID[l])[-1])
+            print('Link orientation (Quat) = ', orientation) 
+            similarity = 1 - np.inner(des, orientation)**2
+            print('Similarity error = ', similarity)
     #print(info)
     # print('Observations = ', obs)
     #print('Done = ', dones)

rexPeriodicRewardEnv.py
@@ -83,10 +83,10 @@ class rexPeriodicRewardEnv(rex_gym_env.RexGymEnv):
         """
         self.phase = 0
 
-        self._gait_type = gait_type        
+        self._gait_type = gait_type        s
         # for observation space bounding 
         self.max_speed = 1.0
-        self.min_speed = 0.2
+        self.min_speed = 0.5 # change back to 0.2 for OLD TD3 model evaluation
         
         self.min_side_speed = 0.0
         self.max_side_speed = 0.0
@@ -108,8 +108,7 @@ class rexPeriodicRewardEnv(rex_gym_env.RexGymEnv):
         super(rexPeriodicRewardEnv,
               self).__init__(urdf_version=urdf_version,
                              accurate_motor_model_enabled=True,
-                             motor_overheat_protection=True,
-                             hard_reset=False,
+                             motor_overheat_protection=False,
                              motor_kp=motor_kp,
                              motor_kd=motor_kd,
                              remove_default_joint_damping=False,
@@ -211,7 +210,7 @@ class rexPeriodicRewardEnv(rex_gym_env.RexGymEnv):
         self.base_vel_next_trans, self.base_vel_next_rot = self.get_base_velocity()
                             
         self._env_step_counter += 1
-        self.phase += self.action_repeat # the cycle length is CYCLE_TIME/time_step so can add 
+        self.phase += self._action_repeat # the cycle length is CYCLE_TIME/time_step so can add 
                                           # how many times an action was repeated
 
         if self.phase > self.cycle_len:
@@ -320,38 +319,49 @@ class rexPeriodicRewardEnv(rex_gym_env.RexGymEnv):
 
         # Speed 
         vx, vy, _ = p.getBaseVelocity(bodyUniqueId=self.rex.quadruped)[0]
+        vx = -vx # in rex, forward is the negative x direction
         x_vel_err = 4*np.abs(vx - self.speed) # higher emphasis on x velocity error
         y_vel_err = np.abs(vy - self.side_speed)
 
         # Orientation
         orient_curr = self.rex.GetBaseOrientation()
-        orient_des = [0, 0, 0, 1]
+        orient_des = [0, 0, 0, 1] # not exact, but shouldn't be too far from this
         orient_err = 6 * (1 - np.inner(orient_curr, orient_des)**2 )
 
         shoulder_orient_des = [0, 0, 0, 1]
         FL_sh, FR_sh, RL_sh, RR_sh = self.get_shoulder_orientation()
 
-        shoulder_err = 10 * ((1 - np.inner(shoulder_orient_des, FL_sh))**2 + 
-                             (1 - np.inner(shoulder_orient_des, FR_sh))**2 +
-                             (1 - np.inner(shoulder_orient_des, RL_sh))**2 + 
-                             (1 - np.inner(shoulder_orient_des, RR_sh))**2)
+        # quaternion similarity: 1 - <q1, q2>**2 == 0 when 100% similar
+        # good when error < 0.01 (individually)
+        # put HUGE penalty on this
+        shoulder_err = 20 * ((1 - np.inner(shoulder_orient_des, FL_sh)**2) + 
+                             (1 - np.inner(shoulder_orient_des, FR_sh)**2) +
+                             (1 - np.inner(shoulder_orient_des, RL_sh)**2) + 
+                             (1 - np.inner(shoulder_orient_des, RR_sh)**2))
 
         # Energy Penalties --------------------------------------------------------------
         energy_penalty = np.abs(np.dot(self.rex.GetMotorTorques(),
-                                       self.rex.GetMotorVelocities())) * self.time_step
+                                       self.rex.GetMotorVelocities())) * self._time_step
 
         # Acceleration
         a_trans, a_rot = self.get_base_accelerations()
         accel_penalty = 0.15 * np.abs(a_trans.sum() + a_rot.sum())
 
-        reward = 0.000 + \
-                 0.250 * np.exp(-orient_err - shoulder_err) +  \
-                 0.225 * np.exp(-foot_penalties) +  \
+        # need to encourage exploration: current issue --> Rex is stuck at origin
+        # because positive rewards all the time
+        # need lim error --> 0, reward > 0 
+
+        beta = -0.75
+
+        reward = beta + \
+                 0.200 * np.exp(-orient_err - shoulder_err) +  \
+                 0.275 * np.exp(-foot_penalties) +  \
                  0.075 * np.exp(-height_err)     +  \
-                 0.200 * np.exp(-x_vel_err)      +  \
+                 0.250 * np.exp(-x_vel_err)      +  \
                  0.100 * np.exp(-y_vel_err)      +  \
-                 0.125 * np.exp(-accel_penalty)  +  \
+                 0.075 * np.exp(-accel_penalty)  +  \
                  0.025 * np.exp(-energy_penalty)
+
 
         return reward
 
@@ -376,8 +386,8 @@ class rexPeriodicRewardEnv(rex_gym_env.RexGymEnv):
         return trans_v, rot_v
 
     def get_base_accelerations(self):
-        trans_a = (np.asarray(self.base_vel_next_trans) - np.asarray(self.base_vel_curr_trans)) / (self.time_step * self.action_repeat)
-        rot_a = (np.asarray(self.base_vel_next_rot) - np.asarray(self.base_vel_curr_rot)) / (self.time_step * self.action_repeat)
+        trans_a = (np.asarray(self.base_vel_next_trans) - np.asarray(self.base_vel_curr_trans)) / (self._time_step * self._action_repeat)
+        rot_a = (np.asarray(self.base_vel_next_rot) - np.asarray(self.base_vel_curr_rot)) / (self._time_step * self._action_repeat)
 
         return trans_a, rot_a
 
@@ -436,10 +446,10 @@ class rexPeriodicRewardEnv(rex_gym_env.RexGymEnv):
         RR_pos_last = np.asarray( self.toe_pos_last['rear_right_toe_pos'] )
 
         # Do velocity calculations
-        FL_toe_vel = np.linalg.norm(( (FL_pos_curr - FL_pos_last)/(self.time_step * self.action_repeat) ), ord=2)
-        FR_toe_vel = np.linalg.norm(( (FR_pos_curr - FR_pos_last)/(self.time_step * self.action_repeat) ), ord=2)
-        RL_toe_vel = np.linalg.norm(( (RL_pos_curr - RL_pos_last)/(self.time_step * self.action_repeat) ), ord=2)
-        RR_toe_vel = np.linalg.norm(( (RR_pos_curr - RR_pos_last)/(self.time_step * self.action_repeat) ), ord=2)
+        FL_toe_vel = np.linalg.norm(( (FL_pos_curr - FL_pos_last)/(self._time_step * self._action_repeat) ), ord=2)
+        FR_toe_vel = np.linalg.norm(( (FR_pos_curr - FR_pos_last)/(self._time_step * self._action_repeat) ), ord=2)
+        RL_toe_vel = np.linalg.norm(( (RL_pos_curr - RL_pos_last)/(self._time_step * self._action_repeat) ), ord=2)
+        RR_toe_vel = np.linalg.norm(( (RR_pos_curr - RR_pos_last)/(self._time_step * self._action_repeat) ), ord=2)
 
         # If any velocities are super tiny, zero them out
         eps = 0.01

rex_gym/envs/rex_gym_env.py
@@ -178,22 +178,22 @@ class RexGymEnv(gym.Env):
         # PD control needs smaller time step for stability.
         if control_time_step is not None:
             self.control_time_step = control_time_step
-            self.action_repeat = action_repeat
-            self.time_step = control_time_step / action_repeat
+            self._action_repeat = action_repeat
+            self._time_step = control_time_step / action_repeat
         else:
             # Default values for time step and action repeat
             if accurate_motor_model_enabled or pd_control_enabled:
-                self.time_step = 0.002
-                self.action_repeat = 5
+                self._time_step = 0.002
+                self._action_repeat = 5
             else:
-                self.time_step = 0.01
-                self.action_repeat = 1
-            self.control_time_step = self.time_step * self.action_repeat
+                self._time_step = 0.01
+                self._action_repeat = 1
+            self.control_time_step = self._time_step * self._action_repeat
         # TODO: Fix the value of self._num_bullet_solver_iterations.
 
-        self.cycle_len = int(TOTAL_CYCLE_TIME/self.time_step)
+        self.cycle_len = int(TOTAL_CYCLE_TIME/self._time_step)
 
-        self._num_bullet_solver_iterations = int(NUM_SIMULATION_ITERATION_STEPS / self.action_repeat)
+        self._num_bullet_solver_iterations = int(NUM_SIMULATION_ITERATION_STEPS / self._action_repeat)
         self._urdf_root = urdf_root
         self._self_collision_enabled = self_collision_enabled
         self._motor_velocity_limit = motor_velocity_limit
@@ -318,7 +318,7 @@ class RexGymEnv(gym.Env):
             self._pybullet_client.resetSimulation()
             self._pybullet_client.setPhysicsEngineParameter(
                 numSolverIterations=int(self._num_bullet_solver_iterations))
-            self._pybullet_client.setTimeStep(self.time_step)
+            self._pybullet_client.setTimeStep(self._time_step)
             self._ground_id = self._pybullet_client.loadURDF("%s/plane.urdf" % self._urdf_root)
             if self._reflection:
                 self._pybullet_client.changeVisualShape(self._ground_id, -1, rgbaColor=[1, 1, 1, 0.8])
@@ -332,9 +332,9 @@ class RexGymEnv(gym.Env):
             else:
                 self.rex = (REX_URDF_VERSION_MAP[self._urdf_version](
                     pybullet_client=self._pybullet_client,
-                    action_repeat=self.action_repeat,
+                    action_repeat=self._action_repeat,
                     urdf_root=self._urdf_root,
-                    time_step=self.time_step,
+                    time_step=self._time_step,
                     self_collision_enabled=self._self_collision_enabled,
                     motor_velocity_limit=self._motor_velocity_limit,
                     pd_control_enabled=self._pd_control_enabled,
@@ -561,7 +561,7 @@ class RexGymEnv(gym.Env):
         # shake_reward = -abs(observation[4])
         energy_reward = -np.abs(
             np.dot(self.rex.GetMotorTorques(),
-                   self.rex.GetMotorVelocities())) * self.time_step
+                   self.rex.GetMotorVelocities())) * self._time_step
         objectives = [forward_reward, energy_reward, drift_reward, shake_reward]
         weighted_objectives = [o * w for o, w in zip(objectives, self._objective_weights)]
         reward = sum(weighted_objectives)
@@ -680,13 +680,13 @@ class RexGymEnv(gym.Env):
         if control_step < simulation_step:
             raise ValueError("Control step should be larger than or equal to simulation step.")
         self.control_time_step = control_step
-        self.time_step = simulation_step
-        self.action_repeat = int(round(control_step / simulation_step))
-        self._num_bullet_solver_iterations = (NUM_SIMULATION_ITERATION_STEPS / self.action_repeat)
+        self._time_step = simulation_step
+        self._action_repeat = int(round(control_step / simulation_step))
+        self._num_bullet_solver_iterations = (NUM_SIMULATION_ITERATION_STEPS / self._action_repeat)
         self._pybullet_client.setPhysicsEngineParameter(
             numSolverIterations=self._num_bullet_solver_iterations)
-        self._pybullet_client.setTimeStep(self.time_step)
-        self.rex.SetTimeSteps(action_repeat=self.action_repeat, simulation_step=self.time_step)
+        self._pybullet_client.setTimeStep(self._time_step)
+        self.rex.SetTimeSteps(action_repeat=self._action_repeat, simulation_step=self._time_step)
 
     @property
     def pybullet_client(self):

test.py
@@ -8,7 +8,8 @@ from rex_gym.envs.gym.standup_env import RexStandupEnv
 from rex_gym.envs.gym.walk_env import RexWalkEnv
 from rexPeriodicRewardEnv import rexPeriodicRewardEnv
 
-env = rexPeriodicRewardEnv(terrain_id='plane', render=True) 
+#env = rexPeriodicRewardEnv(terrain_id='plane', render=True) 
+env = RexWalkEnv(render=True)
 p = env.rex._pybullet_client
 model_id = env.rex.quadruped
 print('model ID: ', model_id)
@@ -88,10 +89,10 @@ def print_toe_velocities(toe_pos_last):
     RR_pos_last = np.asarray( toe_pos_last['rear_right_toe_pos'] )
 
     # Do velocity calculations
-    FL_toe_vel = np.linalg.norm(( (FL_pos_curr - FL_pos_last)/env._time_step ), ord=2)
-    FR_toe_vel = np.linalg.norm(( (FR_pos_curr - FR_pos_last)/env._time_step ), ord=2)
-    RL_toe_vel = np.linalg.norm(( (RL_pos_curr - RL_pos_last)/env._time_step ), ord=2)
-    RR_toe_vel = np.linalg.norm(( (RR_pos_curr - RR_pos_last)/env._time_step ), ord=2)
+    FL_toe_vel = np.linalg.norm(( (FL_pos_curr - FL_pos_last)/env.time_step ), ord=2)
+    FR_toe_vel = np.linalg.norm(( (FR_pos_curr - FR_pos_last)/env.time_step ), ord=2)
+    RL_toe_vel = np.linalg.norm(( (RL_pos_curr - RL_pos_last)/env.time_step ), ord=2)
+    RR_toe_vel = np.linalg.norm(( (RR_pos_curr - RR_pos_last)/env.time_step ), ord=2)
 
     # If any velocities are super tiny, zero them out
     eps = 0.01
@@ -123,10 +124,14 @@ for i in range(rex_joints):
 	name = p.getJointInfo(model_id, i)[12].decode('UTF-8')
 	link_name_to_ID[name] = i
 
+print('Initial state info: ----------------------------------------------------')
 for l in link_name_to_ID.keys():
-    if True:
+    if 'shoulder' in l:
         print('Link name = ', l)
-        print('Link orientation = ', p.getLinkState(bodyUniqueId=model_id, linkIndex=link_name_to_ID[l])[-1]) 
+        orientation = p.getLinkState(bodyUniqueId=model_id, linkIndex=link_name_to_ID[l])[-1]
+        print('Link orientation (Quat) = ', orientation) 
+
+        print('Link orientation (Euler) = ', p.getEulerFromQuaternion(orientation))
 
 # # initialize toe_position logging (in world frame)
 # toe_pos = { 'front_left_toe_pos'  : p.getLinkState(model_id, link_name_to_ID['front_left_toe_link'])[0],
@@ -136,11 +141,23 @@ for l in link_name_to_ID.keys():
 
 #         }       
 
-# time = 0
-# for _ in range(10000):
-#     ob, re, done, ac = env.step(env.action_space.sample()) # take a random action
-#     #print(ac)
-#     #print_contact_info()
-#     #print_toe_velocities(toe_pos)
+time = 0
+des = np.array([0,0,0,1])
+for _ in range(1000):
+    ob, re, done, ac = env.step(env.action_space.sample()) # take a random action
+    print('\n')
+    for l in link_name_to_ID.keys():
+        if 'shoulder' in l:
+            print('Link name = ', l)
+            orientation = np.array(p.getLinkState(bodyUniqueId=model_id, linkIndex=link_name_to_ID[l])[-1])
+            print('Link orientation (Quat) = ', orientation) 
+            similarity = 1 - np.inner(des, orientation)**2
+            print('Similarity = ', similarity)
+            
+
+    #print(p.getBaseVelocity(bodyUniqueId=model_id))
+    #print(ac)
+    #print_contact_info()
+    #print_toe_velocities(toe_pos)
 
 env.close()